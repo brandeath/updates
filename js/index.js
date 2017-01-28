ec826c8e4149865e30091088cc25c067092f62fd
OctoMiao <emptymalei@qq.com>
2017-01-27 17:33:51 -0700

    update footer; fixed wrong link to Tamborra

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@ $(function() {
   $(".light").click(function() {
     $(".feed").removeClass('dark');
   });
-  
+
    var limit = 3;
-  
+
   function feedparser(url,divid) {
   $.ajax({
     type: "GET",
@@ -27,33 +27,33 @@ $(function() {
     }
   });
   }
-  
-   
-  
+
+
+
   var url_graffelt1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AGeorg.G.Raffelt.1';
   feedparser(url_graffelt1, graffelt)
 
   var url_gfuller1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AG.M.Fuller.1'
   feedparser(url_gfuller1, gfuller1)
-  
+
   var url_gfuller2 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AG.M.Fuller.2'
   feedparser(url_gfuller2, gfuller2)
-  
+
   var url_hduan = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AHuaiyu.Duan.1'
   feedparser(url_hduan, hduan)
-  
+
   var url_amirizzi = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AA.Mirizzi.1'
   feedparser(url_amirizzi, amirizzi)
-  
-  
+
+
   var url_jpkneller1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AJ.P.Kneller.1'
   feedparser(url_jpkneller1, jpkneller1)
-  
+
     var url_bdasgupta1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AB.Dasgupta.1'
   feedparser(url_bdasgupta1, bdasgupta1)
 
-var url_itamborra1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AB.Dasgupta.1'
+var url_itamborra1 = 'http://inspirehep.net/rss?ln=en&p=exactauthor%3AI.Tamborra.1'
   feedparser(url_itamborra1, itamborra1)
-  
-  
+
+
 });
